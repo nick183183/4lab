a692c3d55ef856775e17c45905c0985ce5cf8e7a
Пономарев Никита Андреевич <131282140+nick183183@users.noreply.github.com>
2024-12-18 17:56:08 +0400

    Update app.py

--- app.py
+++ app.py
@@ -63,4 +63,6 @@ def index():
     return render_template("index.html", graph_url=graph_url, lambda_values=lambda_values, mu_values=mu_values)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
